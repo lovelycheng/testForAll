cd0172ee86e7c6f33dfdea2516f585ae4c08b914
windside214 <chengtong@treefinance.com.cn>
2022-05-20 11:16:45 +0800

    apollo 自动化生成 properties文件

---
+++ testForApollo/src/main/java/tech/lovelycheng/apollo/test/openapi/MergeTwoApollo.java
@@ -0,0 +1,18 @@
+package tech.lovelycheng.apollo.test.openapi;
+
+/**
+ * @author chengtong
+ * @date 2022/5/19 21:03
+ */
+public class MergeTwoApollo {
+
+    
+
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
